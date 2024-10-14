9d95c7231b85b7789fb9b0723606e6a00e149e32
Jakub Miarka <jakvbmiarka@gmail.com>
2024-10-14 16:08:26 +0200

    Dodaj pliki projektów.

---
+++ AI For Engineering purposes (metaheuristics)/IOptimizationAlgorithm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI_For_Engineering_purposes_metaheuristics
+{
+    public interface IOptimizationAlgorithm
+    {
+        // Nazwa algorytmu
+        string Name { get; set; }
+
+        // Metoda rozpoczynaj ąca rozwi ą zywanie zagadnienia poszukiwania
+        // minimum funkcji celu .
+        // Na pocz ątku sprawdza , czy w lokalizacji jest plik ze stanem algorytmu
+        // w odpowiednim formacie . Jeśli taki plik istnieje , wznawia obliczenia
+        // dla tego stanu ,
+        // W przeciwnym razie zaczyna obliczenia od pocz ą tku .
+        // Zwraca warto ść funkcji celu dla znalezionego rozwi ą zania
+        // ( najlepszego osobnika )
+        double Solve();
+
+        // Właś ciow ść zwracaj ąca tablic ę z najlepszym osobnikiem
+        double[] XBest { get; set; }
+
+        // Właś ciwo ść zwracaj ąca warto ść funkcji dopasowania
+        // dla najlepszego osobnika
+        double FBest { get; set; }
+
+        // Właś ciwo ść zwracaj ąca liczb ę wywo łań funkcji dopasowania
+        int NumberOfEvaluationFitnessFunction { get; set; }
+    }
+}
---
+++ AI For Engineering purposes (metaheuristics)/Metaheuristics/PO.cs
@@ -0,0 +1,61 @@
+﻿using AI_For_Engineering_purposes_metaheuristics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI_For_Engineering_purposes__metaheuristics_.Metaheuristics
+{
+    class PO : IOptimizationAlgorithm
+    {
+
+        //zmienne pomocnicze do "nadzorowania pracy algorytmu
+        private string name = "Puma optimization";
+        private int nOfCalls = 0;
+        private int evaluationTime = 0;
+        private double[] fBestHistory; 
+        private double fBest;
+        private double[] xBest;
+
+
+        //zmienne do opdalenia algorytmu z dana funkcja
+        private int nDimensions;
+        private int nIterations;
+        private int population;
+        private double[] upperBoundaries;
+        private double[] lowerBoundaries;
+        private Func<double[], double> f;
+
+        //parametry algorytmu do "dostrojenia"
+        double PF1, PF2, PF3, U, L, Alpha;
+
+        public PO(int nIterations, int population, double[] upperBoundaries, double[] lowerBoundaries, Func<double[], double> f, double PF1 = 0.5, double PF2 = 0.5, double PF3 = 0.3, double U = 0.4, double L = 0.7, double Alpha = 2)
+        {
+            this.nIterations = nIterations;
+            this.population = population;
+            this.upperBoundaries = upperBoundaries;
+            this.lowerBoundaries = lowerBoundaries;
+            this.f = f;
+            this.PF1 = PF1;
+            this.PF2 = PF2;
+            this.PF3 = PF3;
+            this.U = U;
+            this.L = L;
+            this.Alpha = Alpha;
+            this.nDimensions = upperBoundaries.Length;
+            this.xBest = new double[nDimensions];
+            this.fBestHistory = new double[nIterations];
+        }
+
+        public string Name { get => name; set => name = value;}
+        public double[] XBest { get => xBest; set => xBest = value; }
+        public double FBest { get => fBest; set => fBest = value; }
+        public int NumberOfEvaluationFitnessFunction { get => nOfCalls; set => nOfCalls = value; }
+
+        public double Solve()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

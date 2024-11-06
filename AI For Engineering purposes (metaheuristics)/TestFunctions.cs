c6c104a8f0bab68eebe096678fcebbde60787714
Jakub Miarka <jakvbmiarka@gmail.com>
2024-11-06 18:24:42 +0100

    reworked the way test functions are held

--- AI For Engineering purposes (metaheuristics)/TestFunctions.cs
+++ AI For Engineering purposes (metaheuristics)/TestFunctions.cs
@@ -1,153 +1,150 @@
+using AI_For_Engineering_purposes__metaheuristics_;
+
 namespace AI_For_Engineering_purposes_metaheuristics
 {
-    public class TestFunctions
+
+    class BentCigar: TestFunction
     {
-        public static double[] BentCigarLowerBoundaries(int dimensions)
-        {
-            return FilledArray(dimensions, -10);
-        }
+        public BentCigar(int nd) { this.NDimension = nd; }
 
-        public static double[] BentCigarUpperBoundaries(int dimensions)
-        {
-            return FilledArray(dimensions, 10);
-        }
+        private int nDimension;
+        public int NDimension { get => nDimension; set => nDimension = value; }
 
-        public static double BentCigarFunction(double[] x)
+        public double[] LowerBoundaries => TestFunction.FilledArray(NDimension, -10);
+
+        public double[] UpperBoundaries => TestFunction.FilledArray(NDimension, 10);
+
+        public string Name => "Bent Cigar";
+
+
+        public double function(double[] args)
         {
-            int n = x.Length;
+            int n = args.Length;
             double sum = 0;
             for (int i = 1; i < n; i++)
-                sum += x[i] * x[i];
-            return x[0] * x[0] + Math.Pow(10, 6) * sum;
+                sum += args[i] * args[i];
+            return args[0] * args[0] + Math.Pow(10, 6) * sum;
         }
+    }
 
-        public static double[] RosenbrockLowerBoundaries(int dimensions)
-        {
-            return FilledArray(dimensions, -5);
-        }
+    class Rosenbrock : TestFunction
+    {
+        public Rosenbrock(int nd) { this.NDimension = nd; }
 
-        public static double[] RosenbrockUpperBoundaries(int dimensions)
-        {
-            return FilledArray(dimensions, 10);
-        }
+        private int nDimension;
+        public int NDimension { get => nDimension; set => nDimension = value; }
 
-        public static double RosenbrockFunction(double[] x)
+        public double[] LowerBoundaries => TestFunction.FilledArray(NDimension, -5.12);
+
+        public double[] UpperBoundaries => TestFunction.FilledArray(NDimension, 5.12);
+
+        public string Name => "Rosenbrock";
+
+
+        public double function(double[] args)
         {
-            int n = x.Length;
+            int n = args.Length;
             double sum = 0;
             for (int i = 0; i < n - 1; i++)
-                sum += 100 * Math.Pow(x[i + 1] - x[i] * x[i], 2) + Math.Pow(1 - x[i], 2);
+                sum += 100 * Math.Pow(args[i + 1] - args[i] * args[i], 2) + Math.Pow(1 - args[i], 2);
             return sum;
         }
+    }
 
-        public static double[] RastriginLowerBoundaries(int dimensions)
-        {
-            return FilledArray(dimensions, -5.12);
-        }
+    class Rastrigin : TestFunction
+    {
+        public Rastrigin(int nd) { this.NDimension = nd; }
 
-        public static double[] RastriginUpperBoundaries(int dimensions)
-        {
-            return FilledArray(dimensions, 5.12);
-        }
+        private int nDimension;
+        public int NDimension { get => nDimension; set => nDimension = value; }
 
-        public static double RastriginFunction(double[] x)
+        public double[] LowerBoundaries => TestFunction.FilledArray(NDimension, -5.12);
+
+        public double[] UpperBoundaries => TestFunction.FilledArray(NDimension, 5.12);
+
+        public string Name => "Rastrigin";
+
+
+        public double function(double[] args)
         {
-            int n = x.Length;
+            int n = args.Length;
             double sum = 0;
             for (int i = 0; i < n; i++)
-                sum += x[i] * x[i] - 10 * Math.Cos(2 * Math.PI * x[i]);
+                sum += args[i] * args[i] - 10 * Math.Cos(2 * Math.PI * args[i]);
             return 10 * n + sum;
         }
+    }
 
-        public static double[] SphereLowerBoundaries(int dimensions)
-        {
-            return FilledArray(dimensions, -10000);
-        }
+    class Sphere : TestFunction
+    {
+        public Sphere(int nd) { this.NDimension = nd; }
 
-        public static double[] SphereUpperBoundaries(int dimensions)
-        {
-            return FilledArray(dimensions, 10000);
-        }
+        private int nDimension;
+        public int NDimension { get => nDimension; set => nDimension = value; }
 
-        public static double SphereFunction(double[] x)
+        public double[] LowerBoundaries => TestFunction.FilledArray(NDimension, -10000);
+
+        public double[] UpperBoundaries => TestFunction.FilledArray(NDimension, 10000);
+
+        public string Name => "Sphere";
+
+
+        public double function(double[] args)
         {
             double sum = 0;
-            for (int i = 0; i < x.Length; i++)
+            for (int i = 0; i < args.Length; i++)
             {
-                sum += x[i] * x[i];
+                sum += args[i] * args[i];
             }
             return sum;
         }
+    }
 
-        public static double[] UnknownFunctionLowerBoundaries(int dimensions)
-        {
-            return FilledArray(dimensions, -10);
-        }
+    class UnknownFunction : TestFunction
+    {
+        public UnknownFunction(int nd) { this.NDimension = nd; }
 
-        public static double[] UnknownFunctionUpperBoundaries(int dimensions)
-        {
-            return FilledArray(dimensions, 10);
-        }
+        private int nDimension;
+        public int NDimension { get => nDimension; set => nDimension = value; }
 
-        public static double UnknownFunction(double[] x)
+        public double[] LowerBoundaries => TestFunction.FilledArray(NDimension, -10);
+
+        public double[] UpperBoundaries => TestFunction.FilledArray(NDimension, 10);
+
+        public string Name => "UnknownFunction";
+
+
+        public double function(double[] args)
         {
-            int n = x.Length;
+            int n = args.Length;
             double sum = 0;
             for (int i = 0; i < n; i++)
-                sum += Math.Abs(x[i] * Math.Sin(x[i]) + 0.1 * x[i]);
+                sum += Math.Abs(args[i] * Math.Sin(args[i]) + 0.1 * args[i]);
             return sum;
         }
-
-        public static double[] EggholderLowerBoundaries()
-        {
-            return new double[] { -512, -512 };
-        }
-
-        public static double[] EggholderUpperBoundaries()
-        {
-            return new double[] { 512, 512 };
-        }
-
-        public static double EggholderFunction(double[] x)
-        {
-            return -(x[1] + 47) * Math.Sin(Math.Sqrt(Math.Abs((x[0] / 2) + (x[1] + 47)))) - x[0] * Math.Sin(Math.Sqrt(Math.Abs(x[0] + (x[1] + 47))));
-        }
-
-        public static double[] FilledArray(int size, double value)
-        {
-            double[] a = new double[size];
-            Array.Fill(a, value);
-            return a;
-        }
-
-            return new FitnessFunctionType[]
-            {
-                new FitnessFunctionType
-                {
-                    Name = "Unknown Function",
-                    MinCoordinates = FilledArray(dimensions, -10),
-                    MaxCoordinates = FilledArray(dimensions, 10),
-                    Dimensions = dimensions,
-                    Fn = (double[] x) => {
-                        
-                    }
-                },
-                new FitnessFunctionType
-                {
-                    Name = "Eggholder function",
-                    MinCoordinates = new double[] {-512, -512},
-                    MaxCoordinates = new double[] {512, 512},
-                    Dimensions = 2,
-                    Fn = (double[] x) => {
-                        return -(x[1] + 47) * Math.Sin(Math.Sqrt(Math.Abs((x[0]/2) + (x[1] + 47)))) -x[0]* Math.Sin(Math.Sqrt(Math.Abs(x[0] + (x[1] + 47))));
-                    }
-                },
-                TSFDE_fractional_boundary_fitness_function.GetFitnessFunction(dimensions)
-            };
-        }
-
-        
     }
+
+    class Eggholder : TestFunction
+    {
+        public Eggholder() { this.NDimension = 2; }
+
+        private int nDimension;
+        public int NDimension { get => nDimension; set => nDimension = 2; }
+
+        public double[] LowerBoundaries => new double[] { -512, -512 };
+
+        public double[] UpperBoundaries => new double[] { 512, 512 };
+
+        public string Name => "Eggholder";
+
+
+        public double function(double[] args)
+        {
+            return -(args[1] + 47) * Math.Sin(Math.Sqrt(Math.Abs((args[0] / 2) + (args[1] + 47)))) - args[0] * Math.Sin(Math.Sqrt(Math.Abs(args[0] + (args[1] + 47))));
+        }
+    }
+
+
 }
 
 

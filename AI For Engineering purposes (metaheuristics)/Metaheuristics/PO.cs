fb5a7cb9d575ffebdaa5fcd59b288d41d902942e
dk5042 <damianknopek03@gmail.com>
2024-11-09 18:34:45 +0100

    finished the solve function in puma

--- AI For Engineering purposes (metaheuristics)/Metaheuristics/PO.cs
+++ AI For Engineering purposes (metaheuristics)/Metaheuristics/PO.cs
@@ -1,9 +1,4 @@
 ﻿using AI_For_Engineering_purposes_metaheuristics;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AI_For_Engineering_purposes__metaheuristics_.Metaheuristics
 {
@@ -27,8 +22,7 @@ namespace AI_For_Engineering_purposes__metaheuristics_.Metaheuristics
         private double[] upperBoundaries;
         private double[] lowerBoundaries;
         private Func<double[], double> f;
-        private double[,] args; // tablica argumentow (w artykule Sol.X)
-        private double[] fit; // tablica wartosci (w artykule Sol.Cost)
+        private Puma[] Pumas;
         private Random rnd = new Random();
         //parametry algorytmu do "dostrojenia"
         double PF1, PF2, PF3, U, L, Alpha;
@@ -49,8 +43,7 @@ namespace AI_For_Engineering_purposes__metaheuristics_.Metaheuristics
             this.nDimensions = upperBoundaries.Length;
             this.xBest = new double[nDimensions];
             this.fBestHistory = new double[nIterations];
-            this.args = new double[population, nDimensions];
-            this.fit = new double[population];
+            this.Pumas = new Puma[population];
         }
 
         public string Name { get => name; set => name = value;}
@@ -58,7 +51,7 @@ namespace AI_For_Engineering_purposes__metaheuristics_.Metaheuristics
         public double FBest { get => fBest; set => fBest = value; }
         public int NumberOfEvaluationFitnessFunction { get => nOfCalls; set => nOfCalls = value; }
 
-        private void Exploitation()
+        private Puma[] Exploitation(Puma[] Pumas)
         {
             for (int i = 0; i < population; i++)
             { 
@@ -80,11 +73,11 @@ namespace AI_For_Engineering_purposes__metaheuristics_.Metaheuristics
                     var sum = 0.0;
                     for (int k = 0; k < population; k++)
                     {
-                        sum += args[k, j];
+                        sum += Pumas[j].Position[k];
                     }
                     mBest[j] = (sum / population) / population;
                     S1[j] += rand9;
-                    S2[j] = F1[j] * R * args[i, j] + F2[j] * (1 - R) * xBest[j];
+                    S2[j] = F1[j] * R * Pumas[i].Position[j] + F2[j] * (1 - R) * xBest[j];
                     Xattack[j] = S2[j] / S1[j];
                 }
 
@@ -95,7 +88,7 @@ namespace AI_For_Engineering_purposes__metaheuristics_.Metaheuristics
                     if (rnd.NextDouble() <= L)
                     {
                         for (int j = 0; j < nDimensions; j++)
-                            newArgs[j] = xBest[j] + 2 * rnd.NextDouble() * Math.Exp(beta[j]) * (args[rnd.Next(population), j] - args[i, j]);
+                            newArgs[j] = xBest[j] + 2 * rnd.NextDouble() * Math.Exp(beta[j]) * (Pumas[rnd.Next(population)].Position[j] - Pumas[i].Position[j]);
                     }
                     else
                     {
@@ -107,16 +100,27 @@ namespace AI_For_Engineering_purposes__metaheuristics_.Metaheuristics
                 {
                     int r1 = (int)Math.Round((population - 1) * rnd.NextDouble());
                     for (int j = 0; j < nDimensions; j++)
-                        newArgs[j] = mBest[j] * args[r1, j] - Math.Pow(-1, rnd.Next(2) * args[i, j] / (1 + Alpha * rnd.NextDouble()));
+                        newArgs[j] = mBest[j] * Pumas[r1].Position[j] - Math.Pow(-1, rnd.Next(2) * Pumas[i].Position[j] / (1 + Alpha * rnd.NextDouble()));
                 }
 
                 boundaryControl(newArgs);
 
                 double newFit = callFunction(newArgs);
 
-                if (newFit < fit[i])
-                    fit[i] = newFit;
+                if (newFit < Pumas[i].Fitness)
+                {
+                    Pumas[i].Position = newArgs;
+                    Pumas[i].Fitness = newFit;
+                }
             }
+            return Pumas;
+        }
+
+        // tutaj pracuj tymku
+        private Puma[] Exploration(Puma[] Pumas)
+        {
+            
+            return Pumas;
         }
 
         //Gaussian distribution
@@ -137,7 +141,7 @@ namespace AI_For_Engineering_purposes__metaheuristics_.Metaheuristics
 
         public double Solve()
         {
-            double[] unSelected = { 1.0, 1.0 };
+            double[] UnSelected = { 1.0, 1.0 };
             double F3_Explore = 0.0;
             double F3_Exploit = 0.0;
             double[] Seq_Time_Explore = { 1.0, 1.0, 1.0 };
@@ -146,43 +150,176 @@ namespace AI_For_Engineering_purposes__metaheuristics_.Metaheuristics
             double[] Seq_Cost_Exploit = { 1.0, 1.0, 1.0 };
             double Score_Explore = 0.0;
             double Score_Exploit = 0.0;
-            double[] PF = { 0.5, 0.5, 0.3 };
             List<double> PF_F3 = new List<double>();
             double Mega_Explor = 0.99;
             double Mega_Exploit = 0.99;
+            double[] Costs_Explor = new double[nIterations];
+            double[] Costs_Exploit = new double[nIterations];
 
             initializePopulation();
 
-            int ind = Array.IndexOf(fit, fit.Min());
-            for (int i = 0; i < nDimensions; i++)
-            {
-                XBest[i] = args[ind, i];
-            }
-            FBest = fit[ind];
+            int ind = Array.IndexOf(Pumas, Pumas.OrderBy(p => p.Fitness).First());
+            XBest = Pumas[ind].Position;
+            FBest = Pumas[ind].Fitness;
 
-            bool flagChange = false;
+            bool Flag_Change = false;
+
+            double[] InitialXBest = XBest;
+            double InitialFBest = FBest;
 
             // Unexperienced Phase
             for (; currentIteration < 3; currentIteration++)
             {
+                Puma[] Pumas_explore = Exploration(Pumas);
+                Costs_Explor[currentIteration] = Pumas_explore.Min(p => p.Fitness);
 
+                Puma[] Pumas_exploit = Exploitation(Pumas);
+                Costs_Exploit[currentIteration] = Pumas_exploit.Min(p => p.Fitness);
+
+                Puma[] CombinedPumas = Pumas.Concat(Pumas_explore).Concat(Pumas_exploit).ToArray();
+                Pumas = CombinedPumas.OrderBy(s => s.Fitness).Take(population).ToArray();
+                XBest = Pumas[0].Position;
+                FBest = Pumas[0].Fitness;
+                fBestHistory[currentIteration] = FBest;
             }
 
+            Seq_Cost_Explore[0] = Math.Abs(InitialFBest - Costs_Explor[0]); // Eq(5)
+            Seq_Cost_Exploit[0] = Math.Abs(InitialFBest - Costs_Exploit[0]);    // Eq(8)
+            Seq_Cost_Explore[1] = Math.Abs(Costs_Explor[1] - Costs_Explor[0]);  // Eq(6)
+            Seq_Cost_Exploit[1] = Math.Abs(Costs_Exploit[1] - Costs_Exploit[0]);    // Eq(9)
+            Seq_Cost_Explore[2] = Math.Abs(Costs_Explor[2] - Costs_Explor[1]);  // Eq(7)
+            Seq_Cost_Exploit[2] = Math.Abs(Costs_Exploit[2] - Costs_Exploit[1]);    // Eq(10)
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (Seq_Cost_Explore[i] != 0)
+                    PF_F3.Add(Seq_Cost_Explore[i]);
+                if (Seq_Cost_Exploit[i] != 0)
+                    PF_F3.Add(Seq_Cost_Exploit[i]);
+            }
+
+            var F1_Explor = PF1 * (Seq_Cost_Explore[0] / Seq_Time_Explore[0]);  // Eq(1)
+            var F1_Exploit = PF1 * (Seq_Cost_Exploit[0] / Seq_Time_Exploit[0]); // Eq(2)
+            var F2_Explor = PF2 * ((Seq_Cost_Explore[0] + Seq_Cost_Explore[1] + Seq_Cost_Explore[2]) / (Seq_Time_Explore[0] + Seq_Time_Explore[1] + Seq_Time_Explore[2]));  // Eq(3)
+            var F2_Exploit = PF2 * ((Seq_Cost_Exploit[0] + Seq_Cost_Exploit[1] + Seq_Cost_Exploit[2]) / (Seq_Time_Exploit[0] + Seq_Time_Exploit[1] + Seq_Time_Exploit[2])); // Eq(4)
+
+            // Score calculation
+            Score_Explore = (PF1 * F1_Explor) + (PF2 * F2_Explor);  // Eq(11)
+            Score_Exploit = (PF1 * F1_Exploit) + (PF2 * F2_Exploit);    // Eq(12)
+
+            // Experienced Phase
+            for (; currentIteration < nIterations; currentIteration++)
+            {
+                bool SelectFlag;
+                double[] Count_select = new double[2];
+
+                if (Score_Explore > Score_Exploit)
+                {
+                    // Exploration
+                    SelectFlag = false;
+                    Pumas = Exploration(Pumas);
+                    Count_select = UnSelected;
+                    UnSelected[1] = UnSelected[1] + 1;
+                    UnSelected[0] = 1;
+                    F3_Explore = PF3;
+                    F3_Exploit = F3_Exploit + PF3;
+
+                    Puma TBest = Pumas.OrderBy(p => p.Fitness).First();
+                    Seq_Cost_Explore[2] = Seq_Cost_Explore[1];
+                    Seq_Cost_Explore[1] = Seq_Cost_Explore[0];
+                    Seq_Cost_Explore[0] = Math.Abs(FBest - TBest.Fitness);
+
+                    if (Seq_Cost_Exploit[0] != 0)
+                        PF_F3.Add(Seq_Cost_Explore[0]);
+
+                    if (TBest.Fitness < FBest)
+                    {
+                        XBest = TBest.Position;
+                        FBest = TBest.Fitness;
+                    }
+                }
+                else
+                {
+                    // Exploitation
+                    SelectFlag = true;
+                    Pumas = Exploitation(Pumas);
+                    Count_select = UnSelected;
+                    UnSelected[0] = UnSelected[0] + 1;
+                    UnSelected[1] = 1;
+                    F3_Explore = F3_Explore + PF3;
+                    F3_Exploit = PF3;
+
+                    Puma TBest = Pumas.OrderBy(p => p.Fitness).First();
+                    Seq_Cost_Exploit[2] = Seq_Cost_Exploit[1];
+                    Seq_Cost_Exploit[1] = Seq_Cost_Exploit[0];
+                    Seq_Cost_Exploit[0] = Math.Abs(FBest - TBest.Fitness);
+
+                    if (Seq_Cost_Exploit[0] != 0)
+                        PF_F3.Add(Seq_Cost_Exploit[0]);
+
+                    if (TBest.Fitness < FBest)
+                    {
+                        XBest = TBest.Position;
+                        FBest = TBest.Fitness;
+                    }
+                }
+
+                if (Flag_Change != SelectFlag)
+                {
+                    Flag_Change = SelectFlag;
+                    Seq_Time_Explore[2] = Seq_Time_Explore[1];
+                    Seq_Time_Explore[1] = Seq_Time_Explore[0];
+                    Seq_Time_Explore[0] = Count_select[0];
+                    Seq_Time_Exploit[2] = Seq_Time_Exploit[1];
+                    Seq_Time_Exploit[1] = Seq_Time_Exploit[0];
+                    Seq_Time_Exploit[0] = Count_select[1];
+                }
+
+                F1_Explor = PF1 * (Seq_Cost_Explore[0] / Seq_Time_Explore[0]);    // Eq(14)
+                F1_Exploit = PF1 * (Seq_Cost_Exploit[0] / Seq_Time_Exploit[0]);   // Eq(13)
+                F2_Explor = PF2 * ((Seq_Cost_Explore[0] + Seq_Cost_Explore[1] + Seq_Cost_Explore[2]) / (Seq_Time_Explore[0] + Seq_Time_Explore[1] + Seq_Time_Explore[2]));    // Eq(16)
+                F2_Exploit = PF2 * ((Seq_Cost_Exploit[0] + Seq_Cost_Exploit[1] + Seq_Cost_Exploit[2]) / (Seq_Time_Exploit[0] + Seq_Time_Exploit[1] + Seq_Time_Exploit[2]));   // Eq(15)
+
+                // calculate function value Eq(17) and Eq(18)
+                if (Score_Explore < Score_Exploit)
+                {
+                    Mega_Explor = Math.Max((Mega_Explor - 0.01), 0.01);
+                    Mega_Exploit = 0.99;
+                }
+                else if (Score_Explore > Score_Exploit)
+                {
+                    Mega_Explor = 0.99;
+                    Mega_Exploit = Math.Max((Mega_Exploit - 0.01), 0.01);
+                }
+
+                double lmn_Explore = 1 - Mega_Explor;   // Eq(24)
+                double lmn_Exploit = 1 - Mega_Exploit;  // Eq(22)
+
+                Score_Explore = (Mega_Explor * F1_Explor) + (Mega_Explor * F2_Explor) + (lmn_Explore * (PF_F3.Min() * F3_Explore));  // Eq(20)
+                Score_Exploit = (Mega_Exploit * F1_Exploit) + (Mega_Exploit * F2_Exploit) + (lmn_Exploit * (PF_F3.Min() * F3_Exploit));  // Eq(19)
+
+                fBestHistory[currentIteration] = FBest;
+            }
             return FBest;
         }
 
         private void initializePopulation()
         {
             Random random = new Random();
-            for(int i=0; i<population; i++)
+
+            for (int i = 0; i < population; i++)
             {
-                double[] arg = new double[nDimensions];
-                for(int j=0; j<nDimensions; j++)
+                Pumas[i].Position = new double[nDimensions];
+
+                for (int j = 0; j < nDimensions; j++)
                 {
-                    args[i, j] = random.NextDouble() * (upperBoundaries[j] - lowerBoundaries[j]) + lowerBoundaries[j];
-                    arg[j] = args[i, j];
+                    Pumas[i].Position[j] = lowerBoundaries[j] + rnd.NextDouble() * (upperBoundaries[j] - lowerBoundaries[j]);
                 }
-                fit[i] = callFunction(arg);
+            }
+
+            for (int i = 0; i < population; i++)
+            {
+                Pumas[i].Fitness = callFunction(Pumas[i].Position);
             }
         }
 
@@ -202,5 +339,11 @@ namespace AI_For_Engineering_purposes__metaheuristics_.Metaheuristics
                 }
             }
         }
+    }
+
+    class Puma
+    {
+        public required double[] Position;
+        public double Fitness;
     }
 }

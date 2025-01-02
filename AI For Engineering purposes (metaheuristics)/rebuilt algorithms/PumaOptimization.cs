4f9b14b018381cfadbe24e6c07d28be2337b7213
Jakub Miarka <jakvbmiarka@gmail.com>
2025-01-02 16:31:08 +0100

    started implementing IOptimizationAlgorithm for PO

---
+++ AI For Engineering purposes (metaheuristics)/rebuilt algorithms/PumaOptimization.cs
@@ -0,0 +1,34 @@
+﻿using AI_For_Engineering_purposes__metaheuristics_.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI_For_Engineering_purposes__metaheuristics_.rebuilt_algorithms
+{
+    internal class PumaOptimization : IOptimizationAlgorithm
+    {
+        private string name = "Puma Optimization Algorithm";
+        private double fbest;
+        private double[] xbest;
+        private ParamInfo[] paramInfo;
+        private int n_call = 0;
+
+        public string Name { get => name; set => name = value; }
+        public ParamInfo[] ParamInfo { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IStateWriter writer { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IStateReader reader { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IGeneratePDFReport pdfReportGenerator { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IGenerateTextReport stringReportGenerator { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double[] Xbest { get => xbest; set => xbest = value; }
+        public double Fbest { get => fbest; set => fbest = value; }
+        public int NumberOfEvaluationFitnessFunction { get => n_call; set => n_call = value; }
+
+        public void Solve(fitnessFunction f, double[,] domain, params double[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

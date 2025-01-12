58a4f7264f763f0ca69e97dd7526264bfc5093dd
Jakub Miarka <jakvbmiarka@gmail.com>
2025-01-12 22:01:24 +0100

    added iFunction interface

---
+++ AI For Engineering purposes (metaheuristics)/Interfaces/IFunction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI_For_Engineering_purposes__metaheuristics_.Interfaces
+{
+    interface IFunction
+    {
+        public fitnessFunction Function { get; }
+
+        public float[,] domain { get; set; }
+
+        public string Name { get; }
+
+        public bool IsMultiDimensional { get; }
+
+    }
+}
